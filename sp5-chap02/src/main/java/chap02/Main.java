0decc2ad8c291e9c3169c75b02af6c48bacdfabc
takoyummy <takoyummy@naver.com>
2021-01-13 19:14:00 +0900

    study di

--- sp5-chap02/src/main/java/chap02/Main.java
+++ sp5-chap02/src/main/java/chap02/Main.java
@@ -7,9 +7,12 @@ public class Main {
 	public static void main(String[] args) {
 		
 		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppContext.class);
-		Greeter g  = ctx.getBean("greeter",Greeter.class);
-		String msg = g.greet("스프링");
-		System.out.println(msg);
+		Greeter g1 = ctx.getBean("greeter", Greeter.class);
+		Greeter g2 = ctx.getBean("greeter",Greeter.class);
+		System.out.println("(g1==g2) =" + (g1==g2));
+//		Greeter g  = ctx.getBean("greeter",Greeter.class);
+//		String msg = g.greet("스프링");
+//		System.out.println(msg);
 		ctx.close();
 	}
 
